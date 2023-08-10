d786e8641f362787707e61c334b084559cc295b8
Skanda <sskanda98@gmail.com>
2023-08-10 17:36:22 +0530

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Users from "./users/pages/Users";
+import Places from "./places/pages/Places";
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users></Users>
+        </Route>
+        <Route path="/places/new" exact>
+          <Places></Places>
+        </Route>
+        <Redirect to="/"></Redirect>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
