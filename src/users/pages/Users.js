7378cd18a75177fdeac34aaadf7fa27acb5f9e86
Skanda <sskanda98@gmail.com>
2023-08-13 18:03:40 +0530

    added side bar

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -5,7 +5,7 @@ function Users() {
   const USERS = [
     {
       id: "u1",
-      name: "Max Schwarz",
+      name: "Rahil Ryn",
       image:
         "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
